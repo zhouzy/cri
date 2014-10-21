1c13e814060cb9b77f0c38cdbf696c62c8fa6d8e
zhouzy <zhouzy@channelsoft.com>
2014-10-21 17:43:53 +0800

    easy-bootstrap框架 version:2.0 1.窗口置前 2.窗口移动

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,7 +47,6 @@ module.exports = function (grunt) {
                     ],
                     dest:'script/func/cri/final/cri.js'
                 }]
-
             },
             css:{
                 files:[{
