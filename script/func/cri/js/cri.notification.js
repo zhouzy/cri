acb7c1a0f610b03e90f12d9b28846ea6b065992d
zhouzy <zhouzy@channelsoft.com>
2014-12-01 11:25:06 +0800

    easy-bootstrap框架 version:2.0 1.实现validator

---
+++ script/func/cri/js/cri.notification.js
@@ -0,0 +1,17 @@
+/**
+ * Author zhouzy
+ * Date   2014/10/14
+ * notification 组件
+ *
+ * 依赖Widgets
+ *
+ */
+!function(window){
+
+    cri.error = function(){},
+
+    cri.warn = function(option) {},
+
+    cri.alert = function(option) {}
+
+}(window);
