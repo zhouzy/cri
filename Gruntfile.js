9573e04afcb049a8272591e832a066df7a7f97f3
zhouzy <zhouzy@channelsoft.com>
2014-12-25 16:12:50 +0800

    easy-bootstrap框架 version:2.0 1.api文档

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat'); // 合并 js 文件
     grunt.loadNpmTasks('grunt-contrib-uglify'); // 压缩 js 文件
     grunt.loadNpmTasks('grunt-contrib-watch');  // 动态执行任务
-    grunt.loadNpmTasks('grunt-contrib-qunit');  //js 单元测试
     grunt.loadNpmTasks('grunt-tomcat-deploy');  //发布
     grunt.loadNpmTasks('grunt-zip');  //发布
 
@@ -12,30 +11,23 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         //编译 less 文件(https://github.com/gruntjs/grunt-contrib-less)
-        less: {
-            development: {
-                options: {
+        less:{
+            development:{
+                options:{
                     yuicompress:false
                 },
                 files:{
-                    src:[
-                        "cri/source/less/*.less",
-                        "!cri/source/less/lib.less",
-                        "!cri/source/less/heme-default.less"],
-                    dest:"cri/source/less/cri.css"
+                    "cri/source/cri/cri.css":["cri/source/less/*.less", "!cri/source/less/lib.less", "!cri/source/less/heme-default.less"]
                 }
             },
-            production: {
+            production:{
                 options:{
+                    rootpath:"cri/source/less/",
                     yuicompress:true
                 },
-                files:[{
-                    src:[
-                        "cri/source/less/*.less",
-                        "!cri/source/less/lib.less",
-                        "!cri/source/less/heme-default.less"],
-                    dest:"cri/source/less/cri.css"
-                }]
+                files:{
+                    "cri/source/cri/cri.css":["cri/source/less/*.less", "!cri/source/less/lib.less", "!cri/source/less/heme-default.less"]
+                }
             }
         },
         //合并 js css 文件(文档 https://github.com/gruntjs/grunt-contrib-concat)
@@ -111,11 +103,10 @@ module.exports = function (grunt) {
                 tasks: "zip:cri"
             }
         }
-
     });
 
     // 开发环境不压缩 可调用 `grunt dev`
-    grunt.registerTask('dev', ['less:development','concat']);
+    grunt.registerTask('dev', ['less:development','concat:js','uglify']);
     // 生产环境压缩 可调用 `grunt pro`
     grunt.registerTask('pro', ['less:production','concat:js','uglify']);
     // 注册以外部调用 `grunt`
